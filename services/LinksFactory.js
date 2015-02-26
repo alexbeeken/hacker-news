16c16f3d174da80d84cb2a76a1b56b9a243d13dd
Alex Beeken <aeb242@gmail.com>
2015-02-25 16:18:53 -0800

    Rank links by a combination of age and number of votes.

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -2,9 +2,25 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
   factory.links = [];
   factory.addLink = function() {
-    factory.links.push({ title: factory.linkTitle, url: factory.linkUrl, id: factory.links.length + 1, comments: [] });
+    factory.links.push({
+      title: factory.linkTitle,
+      url: factory.linkUrl,
+      id: factory.links.length + 1,
+      comments: [],
+      points: 0,
+      dateCreated: new Date(),
+      dateNow: Date.now(),
+      orderPost: Date.now(),
+      addVote: function() {
+        this.points++;
+        this.orderPost = this.dateNow + (this.points * 10000);
+      }
+    });
     factory.linkTitle = null;
     factory.linkUrl = null;
   };
+
+
+
   return factory;
 })
